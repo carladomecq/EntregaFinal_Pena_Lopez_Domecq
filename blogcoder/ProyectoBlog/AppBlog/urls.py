76e8bdb1b925fc7151f3fb7a78c7b9f935a7a4d4
Carla Domecq <carlamarianadomecq@gmail.com>
2022-05-07 21:34:16 -0300

    make it better

--- blogcoder/ProyectoBlog/AppBlog/urls.py
+++ blogcoder/ProyectoBlog/AppBlog/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('logout', LogoutView.as_view(template_name='AppBlog/logout.html'), name="Logout"),
     path('editarPerfil', views.editarPerfil, name="EditarPerfil"),
                
-]
+] 
