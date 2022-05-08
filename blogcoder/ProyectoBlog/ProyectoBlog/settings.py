9067abe149b89bba137bf906fb404024ea6b7655
Carla Domecq <carlamarianadomecq@gmail.com>
2022-05-07 21:07:01 -0300

    make it better

--- blogcoder/ProyectoBlog/ProyectoBlog/settings.py
+++ blogcoder/ProyectoBlog/ProyectoBlog/settings.py
@@ -135,6 +135,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/STATIC/'
@@ -148,3 +149,15 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.compressedManifestStaticFileStorage'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
